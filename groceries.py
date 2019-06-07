9b430051dbc17cfd9c90a1ae6f1e577c65d48f2f
hirokiden <hd1043@stern.nyu.edu>
2019-06-07 12:04:57 -0400

    List of goods and price, unsorted

--- groceries.py
+++ groceries.py
@@ -43,3 +43,46 @@ products = [
 
     # python groceries.py
 
+products_count = len(products)
+
+# print(type(products)) --> Determine what class/category products is, in which case is a list
+
+print("--------------")
+print("THERE ARE" + " " + str(products_count) + " " + "PRODUCTS:")
+print("--------------")
+
+
+# products[0]["name"] --> this will print out the name of the first product
+
+
+for i in products:
+    products_name = i["name"]
+    products_price = "{0:.2f}".format(i["price"])
+
+    print("+" + " " + str(products_name) + " " + "($" + str(products_price) + ")")
+
+
+# ###### DESIRED OUTPUT BELOW ########
+# --------------
+# THERE ARE 20 PRODUCTS:
+# --------------
+#  + All-Seasons Salt ($4.99)
+#  + Chocolate Fudge Layer Cake ($18.50)
+#  + Chocolate Sandwich Cookies ($3.50)
+#  + Cut Russet Potatoes Steam N' Mash ($4.25)
+#  + Dry Nose Oil ($21.99)
+#  + Fresh Scent Dishwasher Cleaner ($4.99)
+#  + Gluten Free Quinoa Three Cheese & Mushroom Blend ($3.99)
+#  + Green Chile Anytime Sauce ($7.99)
+#  + Light Strawberry Blueberry Yogurt ($6.50)
+#  + Mint Chocolate Flavored Syrup ($4.50)
+#  + Overnight Diapers Size 6 ($25.50)
+#  + Peach Mango Juice ($1.99)
+#  + Pizza For One Suprema Frozen Pizza ($12.50)
+#  + Pomegranate Cranberry & Aloe Vera Enrich Drink ($4.25)
+#  + Pure Coconut Water With Orange ($3.50)
+#  + Rendered Duck Fat ($9.99)
+#  + Robust Golden Unsweetened Oolong Tea ($2.49)
+#  + Saline Nasal Mist ($16.00)
+#  + Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce ($6.99)
+#  + Sparkling Orange Juice & Prickly Pear Beverage ($2.99)
